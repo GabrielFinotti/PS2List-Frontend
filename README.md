3c2eecc635eb5d642dbeee9417150d0481227c8e
Gabriel H. Finotti <gabrielhenriquefinotti@gmail.com>
2023-11-30 23:36:54 -0300

    Reset

--- README.md
+++
@@ -1,3 +0,0 @@
-# UserList
-
- Site de cadastramento e listagem de usuarios para o modulo de Linguagem de Programação Web
--- src/app/model/interfaces/form-user.ts
+++
@@ -1,6 +0,0 @@
-export interface FormUser {
-  // Modelo de interface do array dos formularios
-  user: string;
-  email: string;
-  password: string;
-}
